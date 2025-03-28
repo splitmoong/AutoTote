7a2c1875213bdeaac854358abda1ffcef45b782a
Akshay Sahasrabuddhey <as1921@srmist.edu.in>
2025-03-28 09:13:39 +0530

    function to get freq itemsets

--- code/association_mining.py
+++ code/association_mining.py
@@ -1,13 +1,21 @@
 import pandas as pd
+#for fp growth (frequent item set generation)
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import fpgrowth
 
 dataset = pd.read_csv("../datasets/groceries_dataset.csv")
 
 
-def user_transactions(user: int):
+def user_transactions(user):
+
+    user = user
     df = dataset
     df = df.groupby(["Member_number", "Date"])["itemDescription"].apply(list).reset_index()
     df.rename(columns={"itemDescription": "Items"}, inplace=True)
-    df = df[df["Member_number"] == user]
+
+    if user != "all":
+        df = df[df["Member_number"] == user]
+
     df = df.drop(columns=["Member_number", "Date"]).reset_index(drop=True)
     return df
 
@@ -21,8 +29,18 @@ def user_transactions(user: int):
 #     df.to_csv("../datasets/exports/transactions.csv", index=False)
 
 #generate frequent itemsets
-def generate_frequent_itemsets():
-    pass
+
+def generate_frequent_itemsets(temp, support):
+    #to convert the transactions into a one hot pandas matrix of items x transactions
+    te = TransactionEncoder()
+    temp = [transaction[0] if isinstance(transaction[0], list) else transaction for transaction in temp]
+    transactions_matrix = te.fit(temp).transform(temp)
+    transactions_df = pd.DataFrame(transactions_matrix, columns=te.columns_)
+
+    #now running fp growth on the matrix
+    freq_itemset = fpgrowth(transactions_df, min_support=support, use_colnames=True)
+    print(freq_itemset)
 
 
-print(user_transactions(1111))
+transactions = user_transactions(user="all")
+generate_frequent_itemsets(transactions.values.tolist(), support=0.1)
